09adc0e440c467373430e73bbbb54550403027f1
Jose Mauricio Toscano Aguas <jtoscanoa.personal@gmail.com>
2025-07-10 21:49:03 -0500

    Add Mensajes SMS button to main menu

---
+++ ProyectoByS/app/src/main/java/com/puropoo/proyectobys/MensajesSmsActivity.java
@@ -0,0 +1,12 @@
+package com.puropoo.proyectobys;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MensajesSmsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensajes_sms);
+    }
+}
